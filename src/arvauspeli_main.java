7847a30a37d59e2bfcd7d745363bcbb668ecafc4
olga23001 <143087063+olga23001@users.noreply.github.com>
2023-10-05 11:38:44 +0300

    Initial commit

---
+++ src/arvauspeli_main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class arvauspeli_main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String name;
+		int counter = 0;
+		int guesses = 0;
+		String stop = "stop";
+		String nothing = "";
+		
+		while (counter < 5)
+		{System.out.println("Guess my name (type stop to exit)");
+		name = in.nextLine();
+		counter++;
+		
+		if (name.equals("Emma"))
+		{
+		System.out.println("Congratulations!");
+		guesses = guesses + 1;
+		
+		if (name.equals("Emma"))
+		System.out.println("You guessed " + guesses + " times.");
+		guesses = guesses + 1;
+		break;
+		}	
+		if (name.equals("stop"))
+		{
+			System.out.println("You guessed " + guesses + " times.");
+			guesses = guesses + 1;
+			break;
+		}
+		
+	
+		else
+		{
+		System.out.print(nothing);
+		guesses = guesses + 1;
+		}
+		
+	
+		
+		}
+		}}
+	
+
